a3c3e08be40ea08187d3d135185ddb4334ad7188
Richard Johnn <rick.johnn@gmail.com>
2018-10-18 12:04:14 -0400

    Refactor to partial views.  Incorporate JavaScript, etc.

---
+++ ScriptCycle.Prototypes/Scripts/Members.js
@@ -0,0 +1,49 @@
+﻿Declare("members", {
+    serverModel: null,
+    model: null,
+    init: function () {       
+
+        if (configuration.serverModel) {
+            configuration.model = ko.observable(ko.mapping.fromJS(members.serverModel));
+        }        
+        ko.applyBindings(members.model);
+    }    
+});
+
+$(function () {  
+    $("#date-of-birth").datepicker({
+        uiLibrary: 'bootstrap4',
+        size: 'small',
+        icons: {
+            rightIcon: '<i class="fa fa-calendar text-primary" aria-hidden="true"></i>'
+        }
+    });
+    $("#group-effective-date").datepicker({
+        uiLibrary: 'bootstrap4',
+        size: 'small',
+        icons: {
+            rightIcon: '<i class="fa fa-calendar text-primary" aria-hidden="true"></i>'
+        }
+    });
+    $("#group-term-date").datepicker({
+        uiLibrary: 'bootstrap4',
+        size: 'small',
+        icons: {
+            rightIcon: '<i class="fa fa-calendar text-primary" aria-hidden="true"></i>'
+        }
+    });
+    $("#subgroup-term-date").datepicker({
+        uiLibrary: 'bootstrap4',
+        size: 'small',
+        icons: {
+            rightIcon: '<i class="fa fa-calendar text-primary" aria-hidden="true"></i>'
+        }
+    });
+    $("#subgroup-effective-date").datepicker({
+        uiLibrary: 'bootstrap4',
+        size: 'small',
+        icons: {
+            rightIcon: '<i class="fa fa-calendar text-primary" aria-hidden="true"></i>'
+        }
+    });
+});
---
+++ ScriptCycle.Prototypes/Scripts/app.js
@@ -0,0 +1,16 @@
+﻿function Declare(namespaceString, objectLiteral) {
+    if (!namespaceString) {
+        throw "namespaceString must be set";
+    }
+    var namespaceObj = window;
+    var namespaceNames = namespaceString.split(".");
+    for (var i = 0; i < namespaceNames.length; i++) {
+        var name = namespaceNames[i];
+        namespaceObj[name] = namespaceObj[name] || {};
+        if (i === namespaceNames.length - 1) {
+            namespaceObj[name] = $.extend(namespaceObj[name], objectLiteral);
+        }
+        namespaceObj = namespaceObj[name];
+    }
+    return objectLiteral;
+}
